4c1b4a0d0927c1b41d256fc0978ca29c53c14b95
Philip Tricca <flihp@twobit.us>
2016-06-21 05:44:51 -0700

    getcommands: Move to resourcemgr/, remove use of SAPI_CLIENT macro, fix error code
    The SAPI_CLIENT macro in this function was used to get the error layer /
    level right when used from different parts of the TSS. But since
    getcommands is only used by the resourcemgr this additional complexity
    isn't necessary. We remove the need for this macro by making this
    function exclusive to the resourcemgr and move the source file to the
    resourcemgr directory.
    
    Additionally the error layer indicator was wrong: the use of
    TSS2_ERROR_LEVEL( TSS2_RESMGR_ERROR_LEVEL ) causes the error level shift
    to happen twice since the TSS2_RESMGR_ERROR_LEVEL in the SAPI headers
    already does the first shift. Applying TSS2_ERROR_LEVEL a second time
    here causes the resmgr error level bits to be shifted 32 bits which is
    completely out of the TPM_RC. This seems to be the only place this error
    is made.
    
    We fix this so that the resmgr layer is identified properly when malloc
    fails. We add a unit test that mocks the malloc function forcing a NULL
    return value.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- resourcemgr/getcommands.c
+++ resourcemgr/getcommands.c
@@ -30,12 +30,6 @@
 #include <stdlib.h>
 #include "sysapi_util.h"
 
-#ifdef SAPI_CLIENT
-TSS2_RC level = TSS2_ERROR_LEVEL( TSS2_APP_ERROR_LEVEL );
-#else
-TSS2_RC level = TSS2_ERROR_LEVEL( TSS2_RESMGR_ERROR_LEVEL );
-#endif    
-
 // Get the TPM 2.0 commands supported by the TPM.
 TSS2_RC GetCommands( TSS2_SYS_CONTEXT *resMgrSysContext, TPML_CCA **supportedCommands )
 {
@@ -45,7 +39,7 @@ TSS2_RC GetCommands( TSS2_SYS_CONTEXT *resMgrSysContext, TPML_CCA **supportedCom
     TPMA_CC *commandPtr;
     TSS2_RC rval = TSS2_RC_SUCCESS;
     UINT32 i;
-    
+
     // First get the number of commands
     rval = Tss2_Sys_GetCapability( resMgrSysContext, 0,
             TPM_CAP_TPM_PROPERTIES, TPM_PT_TOTAL_COMMANDS,
@@ -66,7 +60,7 @@ TSS2_RC GetCommands( TSS2_SYS_CONTEXT *resMgrSysContext, TPML_CCA **supportedCom
     *supportedCommands = (TPML_CCA *)malloc( numCommands * sizeof( TPMA_CC ) + sizeof( UINT32 ) );
     if( !*supportedCommands )
     {
-        rval = TSS2_BASE_RC_INSUFFICIENT_BUFFER + level;
+        rval = TSS2_BASE_RC_INSUFFICIENT_BUFFER + TSS2_RESMGR_ERROR_LEVEL;
         goto returnFromGetCommands;
     }
 
@@ -95,7 +89,7 @@ TSS2_RC GetCommands( TSS2_SYS_CONTEXT *resMgrSysContext, TPML_CCA **supportedCom
             break;
         }
     }
-    
+
 returnFromGetCommands:
     return rval;
 }
@@ -113,7 +107,7 @@ UINT8 GetCommandAttributes( TPM_CC commandCode, TPML_CCA *supportedCommands, TPM
 {
     UINT32 i;
     UINT8 rval = 0;
-    
+
     for( i = 0; i < supportedCommands->count; i++ )
     {
         if( (TPM_CC)( supportedCommands->commandAttributes[i].commandIndex ) == commandCode )
