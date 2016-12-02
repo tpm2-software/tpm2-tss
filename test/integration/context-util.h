b77704ef72aa6d474a82b81134674f66982b411a
Philip Tricca <philip.b.tricca@intel.com>
2016-12-02 11:26:59 -0800

    test: Add integration test "framework".
    This adds a test driver for integration tests in the main.c file.
    Additinoally we provide a mechanism for the test to get TCTI
    configuration data from the environment. Tests should include the
    'test.h' file and implement the function whos prototype is defined
    there.
    
    Conflicts:
    	Makefile.am

---
+++ test/integration/context-util.h
@@ -0,0 +1,18 @@
+#ifndef CONTEXT_UTIL_H
+#define CONTEXT_UTIL_H
+
+#include "sapi/tpm20.h"
+#include "test-options.h"
+
+/**
+ * functions to setup TCTIs and SAPI contexts  using data from the common
+ * options
+ */
+TSS2_TCTI_CONTEXT*    tcti_device_init    (char const         *device_name);
+TSS2_TCTI_CONTEXT*    tcti_socket_init    (char const         *address,
+                                           uint16_t            port);
+TSS2_TCTI_CONTEXT*    tcti_init_from_opts (test_opts_t        *options);
+TSS2_SYS_CONTEXT*     sapi_init_from_opts (test_opts_t        *options);
+void                  sapi_teardown_full  (TSS2_SYS_CONTEXT   *sapi_context);
+
+#endif /* CONTEXT_UTIL_H */
