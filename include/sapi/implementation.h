tpm2-software/tpm2-tss

commit 01e1e12965d747f5d95760baf931850e69294334

Remove unnecessary definitions of various booleans.
These are unused in the code though they are defined in part 2 of the
spec.

Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

include/sapi/implementation.h
@@ -39,13 +39,8 @@
 #undef FALSE
 
 // From TPM 2.0 Part 2: Table 4 - Defines for Logic Values
-
-#define  TRUE     1
-#define  FALSE    0
 #define  YES      1
 #define  NO       0
-#define  SET      1
-#define  CLEAR    0
 
 // From Vendor-Specific: Table 1 - Defines for Processor Values
 

include/sapi/tss2_tpm2_types.h
@@ -37,9 +37,6 @@
 
 #include "implementation.h"
 
-#define SET	1
-#define CLEAR	0
-
 #define    MAX_CAP_DATA         (MAX_CAP_BUFFER-sizeof(TPM_CAP)-sizeof(UINT32))
 #define    MAX_CAP_ALGS         (TPM_ALG_LAST - TPM_ALG_FIRST + 1)
 #define    MAX_CAP_HANDLES      (MAX_CAP_DATA/sizeof(TPM_HANDLE))
@@ -47,14 +44,6 @@
 #define    MAX_TPM_PROPERTIES   (MAX_CAP_DATA/sizeof(TPMS_TAGGED_PROPERTY))
 #define    MAX_PCR_PROPERTIES   (MAX_CAP_DATA/sizeof(TPMS_TAGGED_PCR_SELECT))
 #define    MAX_ECC_CURVES       (MAX_CAP_DATA/sizeof(TPM_ECC_CURVE))
-
-/* Table 4  Defines for Logic Values */
-#define	TRUE	1
-#define	FALSE	0
-#define	YES	1
-#define	NO	0
-#define	SET	1
-#define	CLEAR	0
 
 /* Table 5  Definition of Types for Documentation Clarity */
 typedef	UINT32	TPM_ALGORITHM_ID;	 /* this is the 1.2 compatible form of the TPM_ALG_ID  */

commit 3f001ab8a7ed0babfbcb184ab6fdaed778162937

implementation.h: Remove undef of TRUE and FALSE macros.
This fixes a bug caused by 01e1e12965d747f5d95760baf931850e69294334
which removed the macro defining TRUE and FALSE but didn't remove the
preceding undef. This causes these macros to be undefined by the TSS2
headers even though they may have been defined by some other header
(like glib in the case of the tabrmd).

Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

include/sapi/implementation.h
@@ -35,9 +35,6 @@
 #ifndef _IMPLEMENTATION_H_
 #define _IMPLEMENTATION_H_
 
-#undef TRUE
-#undef FALSE
-
 // From TPM 2.0 Part 2: Table 4 - Defines for Logic Values
 #define  YES      1
 #define  NO       0