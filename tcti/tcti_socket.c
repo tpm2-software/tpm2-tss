82574b6b78d59e183e92d43efd22f6db99de0608
Philip Tricca <philip.b.tricca@intel.com>
2018-03-04 21:47:58 -0800

    libtcti-socket: Refactor CloseSockets function.
    The previous implementation of this function took both of the sockets
    handled by this module. It seems pretty logical to have the function do
    this for only one socket but call it twice. We also update the function
    to take a reference to a SOCKET so that it can clear value once it's been
    closed.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- tcti/tcti_socket.c
+++ tcti/tcti_socket.c
@@ -276,7 +276,8 @@ void SocketFinalize(
     send_sim_session_end (tcti_intel->otherSock);
     send_sim_session_end (tcti_intel->tpmSock);
 
-    CloseSockets (tcti_intel->otherSock, tcti_intel->tpmSock);
+    socket_close (&tcti_intel->otherSock);
+    socket_close (&tcti_intel->tpmSock);
 }
 
 TSS2_RC SocketReceiveTpmResponse(
@@ -467,13 +468,15 @@ static TSS2_RC InitializeMsTpm2Simulator(
 
     rval = PlatformCommand (tctiContext ,MS_SIM_POWER_ON);
     if (rval != TSS2_RC_SUCCESS) {
-        CloseSockets (tcti_intel->otherSock, tcti_intel->tpmSock);
+        socket_close (&tcti_intel->otherSock);
+        socket_close (&tcti_intel->tpmSock);
         return rval;
     }
 
     rval = PlatformCommand (tctiContext, MS_SIM_NV_ON);
     if (rval != TSS2_RC_SUCCESS) {
-        CloseSockets (tcti_intel->otherSock, tcti_intel->tpmSock);
+        socket_close (&tcti_intel->otherSock);
+        socket_close (&tcti_intel->tpmSock);
     }
 
     return rval;
@@ -535,7 +538,8 @@ _InitSocketTcti (
     return TSS2_RC_SUCCESS;
 
 fail_out:
-    CloseSockets (*otherSock, *tpmSock);
+    socket_close (tpmSock);
+    socket_close (otherSock);
 
     return TSS2_TCTI_RC_IO_ERROR;
 }
