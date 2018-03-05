d81fc1a219b59e32a1b3b848b11c453b22b9e56b
Philip Tricca <philip.b.tricca@intel.com>
2018-03-04 21:59:48 -0800

    libtcti-socket: Refactor simulator init function.
    The response code from PlatformCommand is passed back to the caller so
    the caller is made aware of failures from within the
    InitializeMsTpm2Simulator function. The existing code in this module
    catches this error and closes the sockets. Having
    InitializeMsTpm2Simulator close the sockets as well is redundant.
    
    This commit also renames this function to from
    `InitializeMsTpm2Simulator` to `simulator_init` and brings it into line
    with our coding standard.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- tcti/tcti_socket.c
+++ tcti/tcti_socket.c
@@ -459,24 +459,23 @@ retSocketReceiveTpmResponse:
  * don't, an error will be returned from each call till they do but
  * the error will at least be meaningful (TPM2_RC_INITIALIZE).
  */
-static TSS2_RC InitializeMsTpm2Simulator(
-    TSS2_TCTI_CONTEXT *tctiContext
-    )
+static TSS2_RC
+simulator_setup (
+    TSS2_TCTI_CONTEXT *tctiContext)
 {
-    TSS2_TCTI_CONTEXT_INTEL *tcti_intel = tcti_context_intel_cast (tctiContext);
     TSS2_RC rval;
 
+    LOG_TRACE ("Initializing TCTI context 0x%" PRIxPTR,
+               (uintptr_t)tctiContext);
     rval = PlatformCommand (tctiContext ,MS_SIM_POWER_ON);
     if (rval != TSS2_RC_SUCCESS) {
-        socket_close (&tcti_intel->otherSock);
-        socket_close (&tcti_intel->tpmSock);
+        LOG_WARNING ("Failed to send MS_SIM_POWER_ON platform command.");
         return rval;
     }
 
     rval = PlatformCommand (tctiContext, MS_SIM_NV_ON);
     if (rval != TSS2_RC_SUCCESS) {
-        socket_close (&tcti_intel->otherSock);
-        socket_close (&tcti_intel->tpmSock);
+        LOG_WARNING ("Failed to send MS_SIM_NV_ON platform command.");
     }
 
     return rval;
@@ -530,7 +529,7 @@ _InitSocketTcti (
     if (rval != TSS2_RC_SUCCESS) {
         goto fail_out;
     }
-    rval = InitializeMsTpm2Simulator (tctiContext);
+    rval = simulator_setup (tctiContext);
     if (rval != TSS2_RC_SUCCESS) {
         goto fail_out;
     }
