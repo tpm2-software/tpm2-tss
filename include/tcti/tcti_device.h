d26239408f432be8f0cdff8ee9ac3985e8313999
Philip Tricca <philip.b.tricca@intel.com>
2018-03-08 15:06:34 -0800

    tcti: Remove common.h
    This was no longer required when we removed the deprecated init
    functions and the compiler attributes that marked them as deprecated.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- include/tcti/tcti_device.h
+++ include/tcti/tcti_device.h
@@ -28,7 +28,6 @@
 #define TCTI_DEVICE_H
 
 #include <sapi/tss2_tcti.h>
-#include "common.h"
 
 #ifdef __cplusplus
 extern "C" {
--- tcti/tcti.c
+++ tcti/tcti.c
@@ -30,7 +30,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include "tcti/common.h"
 #include "sapi/tpm20.h"
 #include "tcti.h"
 #define LOGMODULE tcti
