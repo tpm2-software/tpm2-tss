a0709104666aa6732261f237c682cd618c5843e7
Juergen Repp <juergen.repp@sit.fraunhofer.de>
2018-03-27 14:26:51 +0200

    Tests: ESAPI Update Object-ChangeAuth
    Somehow there was a return 0 within this test.
    Removed it and fixed the test to work correctly.
    
    Signed-off-by: Andreas Fuchs <andreas.fuchs@sit.fraunhofer.de>

--- test/integration/esys-object-changeauth.int.c
+++ test/integration/esys-object-changeauth.int.c
@@ -82,19 +82,13 @@ test_invoke_esapi(ESYS_CONTEXT * esys_context)
     TPM2B_SENSITIVE_CREATE inSensitivePrimary = {
         .size = 4,
         .sensitive = {
-            .userAuth = {
-                 .size = 0,
-                 .buffer = {0 },
-             },
+            .userAuth = authValuePrimary,
             .data = {
                  .size = 0,
                  .buffer = {0},
              },
         },
     };
-    return 0;
-
-    inSensitivePrimary.sensitive.userAuth = authValuePrimary;
 
     TPM2B_DATA outsideInfo = {
         .size = 0,
@@ -126,25 +120,127 @@ test_invoke_esapi(ESYS_CONTEXT * esys_context)
                            &creationTicket);
     goto_if_error(r, "Error esys create primary", error);
 
+   r = Esys_TR_SetAuth(esys_context, primaryHandle_handle, &authValuePrimary);
+   goto_if_error(r, "Error esys TR_SetAuth ", error);
+
+    TPM2B_AUTH authKey2 = {
+        .size = 6,
+        .buffer = {6, 7, 8, 9, 10, 11}
+    };
+
+    TPM2B_SENSITIVE_CREATE inSensitive2 = {
+        .size = 1,
+        .sensitive = {
+            .userAuth = {
+                 .size = 0,
+                 .buffer = {0}
+             },
+            .data = {
+                 .size = 0,
+                 .buffer = {}
+             }
+        }
+    };
+
+    inSensitive2.sensitive.userAuth = authKey2;
+
+    TPM2B_PUBLIC inPublic2 = {
+        .size = 0,
+        .publicArea = {
+            .type = TPM2_ALG_RSA,
+            .nameAlg = TPM2_ALG_SHA256,
+            .objectAttributes = (TPMA_OBJECT_USERWITHAUTH |
+                                 TPMA_OBJECT_RESTRICTED |
+                                 TPMA_OBJECT_DECRYPT |
+                                 TPMA_OBJECT_FIXEDTPM |
+                                 TPMA_OBJECT_FIXEDPARENT |
+                                 TPMA_OBJECT_SENSITIVEDATAORIGIN),
+
+            .authPolicy = {
+                 .size = 0,
+             },
+            .parameters.rsaDetail = {
+                 .symmetric = {
+                     .algorithm = TPM2_ALG_AES,
+                     .keyBits.aes = 128,
+                     .mode.aes = TPM2_ALG_CFB
+                 },
+                 .scheme = {
+                      .scheme =
+                      TPM2_ALG_NULL,
+                  },
+                 .keyBits = 2048,
+                 .exponent = 65537
+             },
+            .unique.rsa = {
+                 .size = 0,
+                 .buffer = {}
+                 ,
+             }
+        }
+    };
+
+    TPM2B_DATA outsideInfo2 = {
+        .size = 0,
+        .buffer = {}
+        ,
+    };
+
+    TPML_PCR_SELECTION creationPCR2 = {
+        .count = 0,
+    };
+
+    TPM2B_PUBLIC *outPublic2;
+    TPM2B_PRIVATE *outPrivate2;
+    TPM2B_CREATION_DATA *creationData2;
+    TPM2B_DIGEST *creationHash2;
+    TPMT_TK_CREATION *creationTicket2;
+
+    r = Esys_Create(esys_context,
+                    primaryHandle_handle,
+                    ESYS_TR_PASSWORD, ESYS_TR_NONE, ESYS_TR_NONE,
+                    &inSensitive2,
+                    &inPublic2,
+                    &outsideInfo2,
+                    &creationPCR2,
+                    &outPrivate2,
+                    &outPublic2,
+                    &creationData2, &creationHash2, &creationTicket2);
+    goto_if_error(r, "Error esys create ", error);
+
+   ESYS_TR loadedKeyHandle;
+
+    r = Esys_Load(esys_context,
+                  primaryHandle_handle,
+                  ESYS_TR_PASSWORD,
+                  ESYS_TR_NONE,
+                  ESYS_TR_NONE, outPrivate2, outPublic2, &loadedKeyHandle);
+    goto_if_error(r, "Error esys load ", error);
+
+    r = Esys_TR_SetAuth(esys_context, loadedKeyHandle, &authKey2);
+    goto_if_error(r, "Error esys TR_SetAuth ", error);
+
     TPM2B_AUTH newAuth = {.size = 20,
                           .buffer={30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
                                    40, 41, 42, 43, 44, 45, 46, 47, 48, 49}};
 
-    TPM2B_PRIVATE *outPrivate;
+    TPM2B_PRIVATE *outPrivateChangeAuth;
 
     r = Esys_ObjectChangeAuth(esys_context,
-                              ESYS_TR_RH_OWNER,
+                              loadedKeyHandle,
                               primaryHandle_handle,
                               ESYS_TR_PASSWORD,
-                              ESYS_TR_PASSWORD,
+                              ESYS_TR_NONE,
                               ESYS_TR_NONE,
                               &newAuth,
-                              &outPrivate
+                              &outPrivateChangeAuth
                               );
     goto_if_error(r, "Error: ObjectChangeAuth", error);
 
     r = Esys_FlushContext(esys_context, primaryHandle_handle);
     goto_if_error(r, "Error during FlushContext", error);
+
+    return 0;
 
  error:
     return 1;
