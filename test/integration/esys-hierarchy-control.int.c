2a2c274a32c8b1db25dfc64d07f98764f448ca10
Juergen Repp <juergen.repp@sit.fraunhofer.de>
2018-04-12 10:52:35 +0200

    Tests/esapi: Add HierarchyControl
    Signed-off-by: Juergen Repp <juergen.repp@sit.fraunhofer.de>
    Signed-off-by: Andreas Fuchs <andreas.fuchs@sit.fraunhofer.de>

---
+++ test/integration/esys-hierarchy-control.int.c
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ * Copyright 2017-2018, Fraunhofer SIT sponsored by Infineon Technologies AG
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+
+#include "tss2_esys.h"
+
+#include "esys_iutil.h"
+#include "test-esapi.h"
+#define LOGMODULE test
+#include "util/log.h"
+
+/*
+ * Test the ESAPI function Esys_HierarchyControl.
+ * The owner hierarchy will be disabled and with Esys_ClockSet it will
+ * be checked whether the owner hierarchy is disabled
+ */
+int
+test_invoke_esapi(ESYS_CONTEXT * esys_context)
+{
+    uint32_t r = 0;
+
+    ESYS_TR authHandle_handle = ESYS_TR_RH_PLATFORM;
+    TPMI_RH_ENABLES enable = TPM2_RH_OWNER;
+    TPMI_YES_NO state = TPM2_NO;
+
+    r = Esys_HierarchyControl(
+        esys_context,
+        authHandle_handle,
+        ESYS_TR_PASSWORD,
+        ESYS_TR_NONE,
+        ESYS_TR_NONE,
+        enable,
+        state);
+    goto_if_error(r, "Error: HierarchyControl", error);
+
+    ESYS_TR auth_handle = ESYS_TR_RH_OWNER;
+    UINT64 newTime = 0xffffff;
+
+    r = Esys_ClockSet(esys_context,
+                      auth_handle,
+                      ESYS_TR_PASSWORD,
+                      ESYS_TR_NONE,
+                      ESYS_TR_NONE,
+                      newTime);
+    goto_error_if_not_failed(r, "Error: ClockSet", error);
+
+    return 0;
+
+ error:
+    return r;
+}
