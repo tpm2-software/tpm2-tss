183ca32fec17d97741b7d4dfda798fa6b3c13bd1
Juergen Repp <juergen.repp@sit.fraunhofer.de>
2018-04-12 10:08:13 +0200

    Tests/esapi: Fix PCR_AuthValue
    Used to be a fail-test due to PCR1.
    Now it's a success-test.
    Removed sessions along the way.
    
    Signed-off-by: Juergen Repp <juergen.repp@sit.fraunhofer.de>
    Signed-off-by: Andreas Fuchs <andreas.fuchs@sit.fraunhofer.de>

--- test/integration/esys-pcr-auth-value.int.c
+++ test/integration/esys-pcr-auth-value.int.c
@@ -32,39 +32,20 @@
 #define LOGMODULE test
 #include "util/log.h"
 
-/* Test whether Esys_PCR_SetAuthValue fails */
+/*
+ * Test the commands Esys_PCR_SetAuthValue and Esys_PCR_SetAuthPolicy.
+ */
 
 int
 test_invoke_esapi(ESYS_CONTEXT * esys_context)
 {
     uint32_t r = 0;
 
-#ifdef TEST_SESSION
-    ESYS_TR session;
-    TPMT_SYM_DEF symmetric = {.algorithm = TPM2_ALG_AES,
-                              .keyBits = {.aes = 128},
-                              .mode = {.aes = TPM2_ALG_CFB}
-    };
-    TPMA_SESSION sessionAttributes;
-    TPM2B_NONCE *nonceTpm;
-    TPM2B_NONCE nonceCaller = {
-        .size = 20,
-        .buffer = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                   11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
-    };
-
-    memset(&sessionAttributes, 0, sizeof sessionAttributes);
-
-    r = Esys_StartAuthSession(esys_context, ESYS_TR_NONE, ESYS_TR_NONE,
-                              ESYS_TR_NONE, ESYS_TR_NONE, ESYS_TR_NONE,
-                              &nonceCaller,
-                              TPM2_SE_HMAC, &symmetric, TPM2_ALG_SHA1, &session,
-                              &nonceTpm);
-
-    goto_if_error(r, "Error: During initialization of session", error);
-#endif /* TEST_SESSION */
-
-    ESYS_TR  pcrHandle_handle = 1;
+    /*
+     * PCR register 20 belongs to the policy group and the auth value group.
+     * PCRs of these groups can be used for SetAuthValue and SetAuthPolicy.
+     */
+    ESYS_TR  pcrHandle_handle = 20;
 
     TPM2B_DIGEST auth = {
         .size = 20,
@@ -75,19 +56,32 @@ test_invoke_esapi(ESYS_CONTEXT * esys_context)
     r = Esys_PCR_SetAuthValue(
         esys_context,
         pcrHandle_handle,
-#ifdef TEST_SESSION
-        session,
-#else
         ESYS_TR_PASSWORD,
-#endif
         ESYS_TR_NONE,
         ESYS_TR_NONE,
         &auth
         );
-    goto_error_if_not_failed(r, "Error: PCR_SetAuthValue", error);
+    goto_if_error(r, "Error: PCR_SetAuthValue", error);
+
+    TPM2B_DIGEST authPolicy = {
+        .size = 20,
+        .buffer = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                   11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+    };
+
+    r = Esys_PCR_SetAuthPolicy(
+        esys_context,
+        ESYS_TR_RH_PLATFORM,
+        ESYS_TR_PASSWORD,
+        ESYS_TR_NONE,
+        ESYS_TR_NONE,
+        &authPolicy,
+        TPM2_ALG_SHA1,
+        pcrHandle_handle);
+    goto_if_error(r, "Error: PCR_SetAuthPolicy", error);
 
     return 0;
 
  error:
-    return 1;
+    return r;
 }
