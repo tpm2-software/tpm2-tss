7439e1254d9cdb0a4746624ebc5d256a72ffe33c
Tadeusz Struk <tadeusz.struk@intel.com>
2018-09-24 15:05:03 -0700

    sys: fix coverity scan reports in CreateLoaded
    CID 1473601:  Control flow issues  (DEADCODE)
    
    Signed-off-by: Tadeusz Struk <tadeusz.struk@intel.com>

--- src/tss2-sys/api/Tss2_Sys_CreateLoaded.c
+++ src/tss2-sys/api/Tss2_Sys_CreateLoaded.c
@@ -49,18 +49,9 @@ TSS2_RC Tss2_Sys_CreateLoaded_Prepare(
     if (rval)
         return rval;
 
-    if (!inPublic) {
-        rval = Tss2_MU_UINT16_Marshal(0, ctx->cmdBuffer,
-                                      ctx->maxCmdSize,
-                                      &ctx->nextData);
-
-    } else {
-
-        rval = Tss2_MU_TPM2B_TEMPLATE_Marshal(inPublic, ctx->cmdBuffer,
-                                              ctx->maxCmdSize,
-                                              &ctx->nextData);
-    }
-
+    rval = Tss2_MU_TPM2B_TEMPLATE_Marshal(inPublic, ctx->cmdBuffer,
+                                          ctx->maxCmdSize,
+                                          &ctx->nextData);
     if (rval)
         return rval;
 
