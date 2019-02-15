6a05b13a3a9b79473a3792fc934d16f950394ecf
Tadeusz Struk <tadeusz.struk@intel.com>
2019-02-15 12:08:27 -0800

    sys: change the order of sanity checks in Tss2_Sys_GetEncryptParam
    The order needs to be changed for the ESYS to be able to
    check if a given command supports param encryption i.e.
    if the first sized param in a response can be encrypted.
    This will be called by ESYS before sending the command to
    adjust session flags, so it might be out of order, but the
    logic here is to check if the error code is TSS2_SYS_RC_NO_ENCRYPT_PARAM
    and if so clear the TPMA_SESSION_ENCRYPT session flag.
    This check will be valid because the function will be called after
    _Prepare call.
    The spec does not enforce the order in which the checks need to
    be performed, only what error codes can be returned.
    
    Signed-off-by: Tadeusz Struk <tadeusz.struk@intel.com>

--- src/tss2-sys/api/Tss2_Sys_GetEncryptParam.c
+++ src/tss2-sys/api/Tss2_Sys_GetEncryptParam.c
@@ -20,12 +20,14 @@ TSS2_RC Tss2_Sys_GetEncryptParam(
     if (!encryptParamSize || !encryptParamBuffer || !ctx)
         return TSS2_SYS_RC_BAD_REFERENCE;
 
+    if (ctx->encryptAllowed == 0)
+        return TSS2_SYS_RC_NO_ENCRYPT_PARAM;
+
+    if (BE_TO_HOST_16(resp_header_from_cxt(ctx)->tag) == TPM2_ST_NO_SESSIONS)
+        return TSS2_SYS_RC_NO_ENCRYPT_PARAM;
+
     if (ctx->previousStage != CMD_STAGE_RECEIVE_RESPONSE)
         return TSS2_SYS_RC_BAD_SEQUENCE;
-
-    if (ctx->encryptAllowed == 0 ||
-        BE_TO_HOST_16(resp_header_from_cxt(ctx)->tag) == TPM2_ST_NO_SESSIONS)
-        return TSS2_SYS_RC_NO_ENCRYPT_PARAM;
 
     /* Get first parameter, interpret it as a TPM2B and return its size field
      * and a pointer to its buffer area. */
