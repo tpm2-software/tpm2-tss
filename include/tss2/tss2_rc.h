bc76d5a9d67e5c9f3d2f73e8474008cc711099ea
William Roberts <william.c.roberts@intel.com>
2019-07-10 14:04:06 -0500

    tss2-rc: add TSS2_RC decoding library
    This library implements the TCG spec:
      -https://trustedcomputinggroup.org/wp-content/uploads/TCG_TSS_RC_v1p0_r12_pub.pdf
    
    This library allows for the conversion of TSS2_RC's from numerical form
    to a standardized human readable text string.
    
    Fixes: #1444
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

---
+++ include/tss2/tss2_rc.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+#ifndef TSS2_RC_H
+#define TSS2_RC_H
+
+#include <stdint.h>
+
+#include "tss2_tpm2_types.h"
+
+typedef const char *(*TSS2_RC_HANDLER)(TSS2_RC rc);
+
+const char *Tss2_RC_Decode(TSS2_RC rc);
+
+TSS2_RC_HANDLER Tss2_RC_SetHandler(uint8_t layer, const char *name, TSS2_RC_HANDLER handler);
+
+#endif
