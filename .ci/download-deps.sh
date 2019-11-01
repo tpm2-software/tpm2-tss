0ce68b9b190e4571af73ebbfae72b0edeccc27b2
Jonas Witschel <diabonas@gmx.de>
2019-11-01 13:19:55 +0100

    ci: build and install ms-tpm-20-ref
    This is for a proof of concept how to build Microsoft's version of the
    TPM simulator. If we decide to go forward with this, it should be
    directly included into the Dockerfile at tpm2-software-container and
    this commit should be removed.
    
    Signed-off-by: Jonas Witschel <diabonas@gmx.de>

--- .ci/download-deps.sh
+++ .ci/download-deps.sh
@@ -2,6 +2,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 function get_deps() {
-
-    echo "no deps"
+    git clone https://github.com/microsoft/ms-tpm-20-ref.git
+    cd ms-tpm-20-ref/TPMCmd
+    ./bootstrap
+    # Work around a bug in ms-tpm-20-ref until the fix in
+    # https://github.com/microsoft/ms-tpm-20-ref/pull/39 is merged upstream
+    ./configure CPPFLAGS='-DTABLE_DRIVEN_MARSHAL=NO'
+    make
+    make install
 }
