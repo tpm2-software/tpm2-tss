7a2004253e80444142166f5be4cfebcf5ead7c6c
Tadeusz Struk <tadeusz.struk@intel.com>
2020-05-30 20:31:15 -0700

    test: move definitions of dummy structures into a common header
    This removes duplicates and make the code easier to
    change/maintain.
    
    Signed-off-by: Tadeusz Struk <tadeusz.struk@intel.com>

--- test/unit/esys-tcti-rcs.c
+++ test/unit/esys-tcti-rcs.c
@@ -22,6 +22,7 @@
 #define LOGMODULE tests
 #include "util/log.h"
 #include "util/aux_util.h"
+#include "esys-dummy-defs.h"
 
 /**
  * This unit test looks into a set of Esys_<cmd>() functions and tests the
@@ -34,132 +35,6 @@
 
 #define TCTI_TRYAGAINERROR_MAGIC 0x5441455252000000ULL        /* 'TAERR\0' */
 #define TCTI_TRYAGAINERROR_VERSION 0x1
-
-/*
- * Esys handles for dummy session and key objects, and initialization values for
- * other objects, which can be used in ESAPI test calls
- */
-#define DUMMY_TR_HANDLE_POLICY_SESSION  ESYS_TR_MIN_OBJECT
-#define DUMMY_TR_HANDLE_KEY ESYS_TR_MIN_OBJECT+1
-#define DUMMY_TR_HANDLE_NV_INDEX ESYS_TR_MIN_OBJECT+2
-#define DUMMY_TR_HANDLE_HIERARCHY_OWNER ESYS_TR_MIN_OBJECT+3
-#define DUMMY_TR_HANDLE_HIERARCHY_PLATFORM ESYS_TR_MIN_OBJECT+4
-#define DUMMY_TR_HANDLE_PRIVACY_ADMIN ESYS_TR_MIN_OBJECT+5
-#define DUMMY_TR_HANDLE_HMAC_SESSION  ESYS_TR_MIN_OBJECT+6
-#define DUMMY_TR_HANDLE_LOCKOUT ESYS_TR_MIN_OBJECT+7
-#define DUMMY_IN_PUBLIC_DATA { \
-        .size = 0, \
-        .publicArea = { \
-            .type = TPM2_ALG_ECC, \
-            .nameAlg = TPM2_ALG_SHA256, \
-            .objectAttributes = (TPMA_OBJECT_USERWITHAUTH | \
-                                 TPMA_OBJECT_RESTRICTED | \
-                                 TPMA_OBJECT_SIGN_ENCRYPT | \
-                                 TPMA_OBJECT_FIXEDTPM | \
-                                 TPMA_OBJECT_FIXEDPARENT | \
-                                 TPMA_OBJECT_SENSITIVEDATAORIGIN), \
-            .authPolicy = { \
-                 .size = 0, \
-             }, \
-            .parameters.eccDetail = { \
-                 .symmetric = { \
-                     .algorithm = \
-                     TPM2_ALG_NULL, \
-                     .keyBits.aes = \
-                     128, \
-                     .mode.aes = \
-                     TPM2_ALG_ECB, \
-                 }, \
-                 .scheme = { \
-                      .scheme = \
-                      TPM2_ALG_ECDSA, \
-                      .details = { \
-                          .ecdsa = \
-                          {. \
-                           hashAlg \
-                           = \
-                           TPM2_ALG_SHA256}}, \
-                  }, \
-                 .curveID = TPM2_ECC_NIST_P256, \
-                 .kdf = { \
-                      .scheme = TPM2_ALG_NULL, \
-                      .details = {}} \
-             }, \
-            .unique.ecc = { \
-                 .x = {.size = 0,.buffer = {}}, \
-                 .y = {.size = 0,.buffer = {}}, \
-             }, \
-        }, \
-    }
-
-#define DUMMY_TPMT_PUBLIC_PARAMS { \
-        .type = TPM2_ALG_ECC, \
-            .parameters.eccDetail = { \
-            .symmetric = { \
-                 .algorithm = \
-                 TPM2_ALG_NULL, \
-                 .keyBits.aes = \
-                 128, \
-                 .mode.aes = \
-                 TPM2_ALG_ECB, \
-             }, \
-            .scheme = { \
-                 .scheme = \
-                 TPM2_ALG_ECDSA, \
-                 .details = { \
-                     .ecdsa = \
-                     {. \
-                      hashAlg \
-                      = \
-                      TPM2_ALG_SHA256}}, \
-             }, \
-            .curveID = TPM2_ECC_NIST_P256, \
-            .kdf = { \
-                 .scheme = TPM2_ALG_NULL, \
-                 .details = {}} \
-        } \
-    }
-
-#define DUMMY_2B_DATA(NAME)  { \
-        .size = 20, \
-        NAME = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, \
-                   11, 12, 13, 14, 15, 16, 17, 18, 19, 20} \
-    }
-
-#define DUMMY_2B_DATA16(NAME)  { \
-        .size = 16, \
-        NAME = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, \
-                   11, 12, 13, 14, 15, 16 } \
-    }
-
-#define DUMMY_2B_DATA0 { \
-        .size = 0, \
-        .buffer = {}, \
-    }
-
-#define DUMMY_SYMMETRIC {.algorithm = TPM2_ALG_AES, \
-        .keyBits = {.aes = 128}, \
-        .mode = {.aes = TPM2_ALG_CFB} \
-    }
-
-#define DUMMY_TPMT_TK_AUTH { .tag = TPM2_ST_AUTH_SIGNED , .hierarchy = TPM2_RH_OWNER, .digest = {0} }
-
-#define DUMMY_TPMT_TK_CREATION { .tag = TPM2_ST_CREATION , .hierarchy = TPM2_RH_OWNER, .digest = {0} }
-
-#define DUMMY_TPMT_TK_VERIFIED { .tag = TPM2_ST_VERIFIED , .hierarchy = TPM2_RH_OWNER, .digest = {0} }
-
-#define DUMMY_TPMT_TK_HASHCHECK { .tag = TPM2_ST_HASHCHECK , .hierarchy = TPM2_RH_OWNER, .digest = {0} }
-
-#define DUMMY_RSA_DECRYPT { .scheme = TPM2_ALG_NULL }
-
-#define DUMMY_TPMT_SIGNATURE { \
-        .sigAlg = TPM2_ALG_RSAPSS, \
-        .signature = { \
-            .rsapss = { \
-                 .hash = TPM2_ALG_SHA1, .sig= {0} \
-             } \
-        } \
-    };
 
 typedef struct {
     uint64_t magic;
