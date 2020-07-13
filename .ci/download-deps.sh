af55dd8160a9a4188b3d44cc4deadee4be8aab21
Tadeusz Struk <tadeusz.struk@intel.com>
2020-07-13 11:39:58 -0700

    build: add libgcrypt dependency
    Signed-off-by: Tadeusz Struk <tadeusz.struk@intel.com>

--- .ci/download-deps.sh
+++ .ci/download-deps.sh
@@ -3,5 +3,13 @@
 
 function get_deps() {
 
-	echo "no deps"
+	echo "Download deps for $DOCKER_TAG"
+
+	if [ "$DOCKER_TAG" = "fedora-30" ]; then
+		yum -y install libgcrypt-devel
+	elif [ "$DOCKER_TAG" = "opensuse-leap" ]; then
+		zypper -n in libgcrypt-devel
+	elif [ "$DOCKER_TAG" = "ubuntu-16.04" -o "$DOCKER_TAG" = "ubuntu-18.04" ]; then
+		apt-get -y install libgcrypt20-dev
+	fi
 }
