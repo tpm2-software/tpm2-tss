6e508670b3b7d4e49b52ab591fb76db49aaf3135
William Roberts <william.c.roberts@intel.com>
2020-07-18 11:08:15 -0500

    esys: make hiearchy API value change backwards compat
    Since:
      - 9d48cc7 esys: change the hierarchy type according to the spec
    
    The interface types changed from TPMI_RH_HIERARCHY and TPMI_RH_ENABLES
    to ESYS_TR, which from the ABI perspective nothing changed, asthey are
    still just UINT32. However, ESYS_TR's do not equal the older TPM2_RH
    values they replaced. However, their is no overlap, so we can detect
    when a non ESYS_TR hiearchy value is passed and accept it. However, we
    want to be noisy about it and the resulting logs will result:
    
    ERROR:esys:src/tss2-esys/esys_iutil.c:389:iesys_handle_to_tpm_handle() Error: Esys invalid ESAPI handle (40000001).
    WARNING:esys:src/tss2-esys/esys_iutil.c:411:iesys_is_platform_handle() Convert handle from TPM2_RH to ESYS_TR, got: 0x40000001
    
    This will allow users/distro's to easily upgrade packages without having
    to immediatley make code changes. However, they will be encouraged to
    use the proper handle values.
    
    Fixes: #1750
    
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- src/tss2-esys/api/Esys_LoadExternal.c
+++ src/tss2-esys/api/Esys_LoadExternal.c
@@ -161,8 +161,12 @@ Esys_LoadExternal_Async(
     }
 
     r = iesys_handle_to_tpm_handle(hierarchy, &tpm_hierarchy);
-    if (r != TSS2_RC_SUCCESS)
-        return r;
+    if (r != TSS2_RC_SUCCESS) {
+        if (!iesys_is_platform_handle(hierarchy)) {
+            return r;
+        }
+        tpm_hierarchy = hierarchy;
+    }
 
     r = iesys_check_sequence_async(esysContext);
     if (r != TSS2_RC_SUCCESS)
