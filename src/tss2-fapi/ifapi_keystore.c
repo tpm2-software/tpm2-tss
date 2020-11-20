tpm2-software/tpm2-tss

commit 05d9ebd1c3ffe7011fc5062211c3a7013f9b33df

FAPI: Add provisioning check.
It will be checked whether the profile directory exists for expanded
pathnames which start with a profile name.
If the profile directory does not exist TSS2_FAPI_RC_NOT_PROVISIONED
will be returned by Fapi_List, and the function which computes absolute
pathnames to read objects from the keystore.

Signed-off-by: Juergen Repp <juergen.repp@sit.fraunhofer.de>

src/tss2-fapi/ifapi_keystore.c
@@ -510,6 +510,7 @@ rel_path_to_abs_path(
 {
     TSS2_RC r;
     char *directory = NULL;
+    bool provision_check_ok;
 
     /* First expand path in user directory  */
     r = expand_path(keystore, rel_path, &directory);
@@ -530,6 +531,19 @@ rel_path_to_abs_path(
         if (ifapi_io_path_exists(*abs_path)) {
             r = TSS2_RC_SUCCESS;
             goto cleanup;
+        }
+
+        /* Check whether provisioning was made for the path profile. */
+        r = ifapi_check_provisioned(keystore, rel_path, &provision_check_ok);
+        goto_if_error(r, "Provisioning check.", cleanup);
+
+        if (provision_check_ok) {
+            goto_error(r, TSS2_FAPI_RC_PATH_NOT_FOUND,
+            "Path not found: %s.", cleanup, rel_path);
+        } else {
+            goto_error(r, TSS2_FAPI_RC_NOT_PROVISIONED,
+                       "FAPI not provisioned for path: %s.",
+                       cleanup, rel_path);
         }
 
         /* Check type of object which does not exist. */
@@ -1768,4 +1782,66 @@ ifapi_cleanup_ifapi_object(
             object->objectType = IFAPI_OBJ_NONE;
         }
     }
+}
+
+/** Check whether profile directory exists for a fapi path.
+ *
+ * It will be checked whether a profile directory exists for a path which starts
+ * with a profile name after fapi pathname expansion.
+ *
+ * @param[in] keystore The key directories and default profile.
+ * @param[in] rel_path The relative path to be checked.
+ * @param[out] ok The boolean value whether the check ok.
+ * @retval TSS2_RC_SUCCESS if the check could be made.
+ * @retval TSS2_FAPI_RC_MEMORY: if memory could not be allocated to compute
+ * the absolute paths.
+ */
+TSS2_RC
+ifapi_check_provisioned(
+    IFAPI_KEYSTORE *keystore,
+    const char *rel_path,
+    bool *ok)
+{
+    TSS2_RC r = TSS2_RC_SUCCESS;
+    char *directory = NULL;
+    char *profile_dir = NULL;
+    char *end_profile;
+
+    *ok = false;
+
+    /* First expand path in user directory  */
+    r = expand_path(keystore, rel_path, &directory);
+    goto_if_error(r, "Expand path", cleanup);
+
+    /* Check whether the path starts with a profile. */
+    if (directory && (strncmp(directory, "P_", 2) != 0 || strncmp(directory, "/P_", 2) != 0)) {
+        end_profile = strchr(&directory[1], '/');
+        if (end_profile) {
+            end_profile[0] = '\0';
+        }
+        /* Compute user path of the profile. */
+        r = ifapi_asprintf(&profile_dir, "%s/%s", keystore->userdir, directory);
+        goto_if_error2(r, "Profile path could not be created.", cleanup);
+
+         if (ifapi_io_path_exists(profile_dir)) {
+             *ok = true;
+             goto cleanup;
+         }
+         /* Compute system path of the profile. */
+         SAFE_FREE(profile_dir);
+         r = ifapi_asprintf(&profile_dir, "%s/%s", keystore->systemdir, directory);
+         goto_if_error2(r, "Profile path could not be created.", cleanup);
+
+         if (ifapi_io_path_exists(profile_dir)) {
+             *ok = true;
+             goto cleanup;
+         }
+    } else {
+        /* No check needed because no profile found in the path. */
+        *ok = true;
+    }
+ cleanup:
+    SAFE_FREE(profile_dir);
+    SAFE_FREE(directory);
+    return r;
 }

src/tss2-fapi/api/Fapi_List.c
@@ -169,6 +169,7 @@ Fapi_List_Finish(
     char        **pathList)
 {
     LOG_TRACE("called for context:%p", context);
+    bool provision_check_ok;
 
     TSS2_RC r = TSS2_RC_SUCCESS;
     size_t sizePathList = 0;
@@ -188,7 +189,7 @@ Fapi_List_Finish(
     goto_if_error(r, "get entities.", cleanup);
 
     if (numPaths == 0)
-        goto cleanup;
+        goto check_provisioning;
 
     /* Determine size of char string to be returnded */
     for (size_t i = 0; i < numPaths; i++)
@@ -208,18 +209,29 @@ Fapi_List_Finish(
             strcat(*pathList, IFAPI_LIST_DELIM);
     }
 
+ check_provisioning:
+    if (numPaths == 0 && (r == TSS2_RC_SUCCESS)) {
+        if (command->searchPath && (strcmp(command->searchPath,"/") == 0
+                                    || strcmp(command->searchPath,"") == 0)) {
+            LOG_WARNING("Path not found: %s", command->searchPath);
+            r = TSS2_FAPI_RC_NOT_PROVISIONED;
+        } else {
+            r = ifapi_check_provisioned(&context->keystore, command->searchPath, &provision_check_ok);
+            goto_if_error(r, "Provisioning check.", cleanup);
+
+            if (provision_check_ok) {
+                LOG_WARNING("Path not found: %s", command->searchPath);
+                r = TSS2_FAPI_RC_PATH_NOT_FOUND;
+            } else {
+                LOG_WARNING("Profile of path not provisioned: %s", command->searchPath);
+                r = TSS2_FAPI_RC_NOT_PROVISIONED;
+            }
+        }
+    }
     LOG_TRACE("finished");
 
 cleanup:
     /* Cleanup any intermediate results and state stored in the context. */
-    if (numPaths == 0 && (r == TSS2_RC_SUCCESS)) {
-        if (command->searchPath && strcmp(command->searchPath,"/") !=0
-            && strcmp(command->searchPath,"") !=0) {
-            LOG_WARNING("Path not found: %s", command->searchPath);
-        } else {
-            LOG_WARNING("FAPI not provisioned.");
-        }
-    }
     if (numPaths > 0) {
         for (size_t i = 0; i < numPaths; i++){
             SAFE_FREE(pathArray[i]);

src/tss2-fapi/ifapi_keystore.h
@@ -280,4 +280,10 @@ void
 ifapi_cleanup_ifapi_object(
     IFAPI_OBJECT *object);
 
+TSS2_RC
+ifapi_check_provisioned(
+    IFAPI_KEYSTORE *keystore,
+    const char *rel_path,
+    bool *ok);
+
 #endif /* IFAPI_KEYSTORE_H */

test/integration/fapi-get-random.int.c
@@ -42,6 +42,7 @@ test_fapi_get_random(FAPI_CONTEXT *context)
     size_t  bytesRequested = sizeof(TPMU_HA) + 10;
     uint8_t *randomBytes = NULL;
 
+
     r = Fapi_Provision(context, NULL, NULL, NULL);
     goto_if_error(r, "Error Fapi_Provision", error);
 

commit ff28d2d121f98079164e6fc5b3317afb938ffb9a

FAPI: fix provisioning check in rel_path_to_abs_path()
Commit 05d9ebd1c3ffe7011fc5062211c3a7013f9b33df added a provisioning check to
rel_path_to_abs_path() that always returns early with
TSS2_FAPI_RC_PATH_NOT_FOUND if the FAPI is provisioned. This leads to dead code
in the rest of the function and possibly wrong return codes, breaking software
that relies on a return value of TSS2_FAPI_RC_KEY_NOT_FOUND instead of
TSS2_FAPI_RC_PATH_NOT_FOUND.

Fixes: 05d9ebd1c3ffe7011fc5062211c3a7013f9b33df
Signed-off-by: Jonas Witschel <diabonas@gmx.de>

src/tss2-fapi/ifapi_keystore.c
@@ -537,10 +537,7 @@ rel_path_to_abs_path(
         r = ifapi_check_provisioned(keystore, rel_path, &provision_check_ok);
         goto_if_error(r, "Provisioning check.", cleanup);
 
-        if (provision_check_ok) {
-            goto_error(r, TSS2_FAPI_RC_PATH_NOT_FOUND,
-            "Path not found: %s.", cleanup, rel_path);
-        } else {
+        if (!provision_check_ok) {
             goto_error(r, TSS2_FAPI_RC_NOT_PROVISIONED,
                        "FAPI not provisioned for path: %s.",
                        cleanup, rel_path);