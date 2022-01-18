8e6b2bd24de22f4cbeb705d7a1baae66f5eb3433
William Roberts <william.c.roberts@intel.com>
2022-01-18 12:04:21 -0600

    ifapi_policyeval_instantiate_finish: check callback set
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- src/tss2-fapi/ifapi_policy_instantiate.c
+++ src/tss2-fapi/ifapi_policy_instantiate.c
@@ -159,6 +159,11 @@ set_pem_key_param(
          return_error2(TSS2_FAPI_RC_BAD_TEMPLATE, "No path for policy %s", template); \
      }
 
+#define CHECK_CALLBACK(callback, name) \
+    if (!callback) { \
+        return_error2(TSS2_FAPI_RC_BAD_VALUE, "Callback %s was NULL", name) \
+    }
+
 /** Finalize  instantiation a policy template.
  *
  * All needed asyncroous callbacks will be executed for all policy elements offset
@@ -223,6 +228,7 @@ ifapi_policyeval_instantiate_finish(
                 break;
             }
             CHECK_TEMPLATE_PATH(pol_element->element.PolicySigned.keyPath, "PolicySigned");
+            CHECK_CALLBACK(context->callbacks.cbpublic, "cbpublic");
 
             /* Public info will be added to policy. */
             r = context->callbacks.cbpublic(pol_element->element.PolicySigned.keyPath,
@@ -238,6 +244,10 @@ ifapi_policyeval_instantiate_finish(
         case POLICYNAMEHASH:
             /* Set index of last name to be computed. */
             i_last = pol_element->element.PolicyNameHash.count - 1;
+            if (pol_element->element.PolicyNameHash.objectNames[i_last].size) {
+                CHECK_CALLBACK(context->callbacks.cbname, "cbname");
+            }
+
             while (!pol_element->element.PolicyNameHash.objectNames[i_last].size) {
                 /* Not all object names have been computed or were initialized */
                 size_t i = pol_element->element.PolicyNameHash.i;
@@ -258,6 +268,7 @@ ifapi_policyeval_instantiate_finish(
                 break;
             }
             CHECK_TEMPLATE_PATH(pol_element->element.PolicySecret.objectPath, "PolicySecret");
+            CHECK_CALLBACK(context->callbacks.cbname, "cbname");
             /* Object name will be added to policy. */
             r = context->callbacks.cbname(pol_element->element.PolicySecret.objectPath,
                                           &pol_element->element.PolicySecret.objectName,
@@ -273,6 +284,7 @@ ifapi_policyeval_instantiate_finish(
                 /* PCR values already defined */
                 break;
             }
+            CHECK_CALLBACK(context->callbacks.cbpcr, "cbpcr");
             /* Current values of PCRs will be used for policy */
             r = context->callbacks.cbpcr(&pol_element->element.PolicyPCR.currentPCRs,
                                          &pol_element->element.PolicyPCR.currentPCRandBanks,
@@ -295,6 +307,7 @@ ifapi_policyeval_instantiate_finish(
             }
 
             CHECK_TEMPLATE_PATH(pol_element->element.PolicyNV.nvPath, "PolicyNv");
+            CHECK_CALLBACK(context->callbacks.cbnvpublic, "cbnvpublic");
             /* Object name will be added to policy. */
             r = context->callbacks.cbnvpublic(pol_element->element.PolicyNV.nvPath,
                                               &pol_element->element.PolicyNV.nvPublic,
@@ -318,7 +331,7 @@ ifapi_policyeval_instantiate_finish(
 
             CHECK_TEMPLATE_PATH(pol_element->element.PolicyDuplicationSelect.newParentPath,
                                 "PolicyDuplicationselect");
-
+            CHECK_CALLBACK(context->callbacks.cbpublic, "cbpublic");
             /* Public info will be added to policy. */
             r = context->callbacks.cbpublic(
                      pol_element->element.PolicyDuplicationSelect.newParentPath,
@@ -345,6 +358,7 @@ ifapi_policyeval_instantiate_finish(
 
             CHECK_TEMPLATE_PATH(pol_element->element.PolicyAuthorizeNv.nvPath,
                                 "PolicyAuthorizeNv");
+            CHECK_CALLBACK(context->callbacks.cbnvpublic, "cbnvpublic");
             /* Object name will be added to policy. */
             r = context->callbacks.cbnvpublic(pol_element->element.PolicyAuthorizeNv.nvPath,
                                               &pol_element->element.PolicyAuthorizeNv.nvPublic,
@@ -383,6 +397,7 @@ ifapi_policyeval_instantiate_finish(
             }
 
             CHECK_TEMPLATE_PATH(pol_element->element.PolicyAuthorize.keyPath, "PolicyAuthorize");
+            CHECK_CALLBACK(context->callbacks.cbpublic, "cbpublic");
 
             /* Object public data will be added to policy. */
             r = context->callbacks.cbpublic(pol_element->element.PolicyAuthorize.keyPath,
