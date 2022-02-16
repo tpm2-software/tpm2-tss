tpm2-software/tpm2-tss

commit a94db7392c060734ece27fd49e1c0680012988cd

FAPI: Fix usage of curl url.
Errors did occur for urls which where not escaped since curl version 7.80. Now the escaping
is executed before executing the download.

Signed-off-by: Juergen Repp <juergen.repp@sit.fraunhofer.de>

src/tss2-fapi/ifapi_helpers.c
@@ -2504,6 +2504,9 @@ ifapi_get_curl_buffer(unsigned char * url, unsigned char ** buffer,
                           size_t *buffer_size) {
     int ret = -1;
     struct CurlBufferStruct curl_buffer = { .size = 0, .buffer = NULL };
+#ifdef CURLU_ALLOW_SPACE
+    CURLU *urlp = NULL;
+#endif
 
     CURLcode rc = curl_global_init(CURL_GLOBAL_DEFAULT);
     if (rc != CURLE_OK) {
@@ -2517,7 +2520,23 @@ ifapi_get_curl_buffer(unsigned char * url, unsigned char ** buffer,
         goto out_global_cleanup;
     }
 
+#ifdef CURLU_ALLOW_SPACE
+    urlp = curl_url();
+    if (!urlp) {
+        LOG_ERROR("curl_url failed.");
+        goto out_easy_cleanup;
+    }
+    rc = curl_url_set(urlp, CURLUPART_URL, url, CURLU_ALLOW_SPACE | CURLU_URLENCODE);
+    if (rc != CURLE_OK) {
+        LOG_ERROR("curl_url_set for CURUPART_URL failed: %s",
+                  curl_easy_strerror(rc));
+        goto out_easy_cleanup;
+    }
+    rc = curl_easy_setopt(curl, CURLOPT_CURLU, urlp);
+#else
     rc = curl_easy_setopt(curl, CURLOPT_URL, url);
+#endif
+
     if (rc != CURLE_OK) {
         LOG_ERROR("curl_easy_setopt for CURLOPT_URL failed: %s",
                 curl_easy_strerror(rc));
@@ -2565,6 +2584,10 @@ ifapi_get_curl_buffer(unsigned char * url, unsigned char ** buffer,
     ret = 0;
 
 out_easy_cleanup:
+#ifdef CURLU_ALLOW_SPACE
+    if (urlp)
+        curl_url_cleanup(urlp);
+#endif
     if (ret != 0)
         free(curl_buffer.buffer);
     curl_easy_cleanup(curl);

commit cf8cff8e59e39bfe9491509f071e88170eb274dc

FAPI: fix curl_url_set call
- The third argument of curl_url_set is of type const char *, leading to the
following compiler warning/error with GCC 11.2.0 and curl 7.81.0:

rc/tss2-fapi/ifapi_helpers.c:2529:44: error: pointer targets in passing argument 3 of 'curl_url_set' differ in signedness [-Werror=pointer-sign]
 2529 |     rc = curl_url_set(urlp, CURLUPART_URL, url, CURLU_ALLOW_SPACE | CURLU_URLENCODE);
      |                                            ^~~
      |                                            |
      |                                            unsigned char *

/usr/include/curl/urlapi.h:132:48: note: expected 'const char *' but argument is of type 'unsigned char *'
  132 |                                    const char *part, unsigned int flags);
      |                                    ~~~~~~~~~~~~^~~~

- The return value of curl_url_set is of type CURLUcode and needs to be
interpreted using curl_url_strerror instead of curl_easy_strerror. This leads
to the following compiler warning/error:

src/tss2-fapi/ifapi_helpers.c:2529:8: error: implicit conversion from 'CURLUcode' to 'CURLcode' [-Werror=enum-conversion]
 2529 |     rc = curl_url_set(urlp, CURLUPART_URL, url, CURLU_ALLOW_SPACE | CURLU_URLENCODE);
      |        ^

Fixes: a94db7392c060734ece27fd49e1c0680012988cd ("FAPI: Fix usage of curl url.")
Signed-off-by: Jonas Witschel <git@diabonas.de>

src/tss2-fapi/ifapi_helpers.c
@@ -2526,10 +2526,11 @@ ifapi_get_curl_buffer(unsigned char * url, unsigned char ** buffer,
         LOG_ERROR("curl_url failed.");
         goto out_easy_cleanup;
     }
-    rc = curl_url_set(urlp, CURLUPART_URL, url, CURLU_ALLOW_SPACE | CURLU_URLENCODE);
-    if (rc != CURLE_OK) {
+    CURLUcode url_rc;
+    url_rc = curl_url_set(urlp, CURLUPART_URL, (const char *)url, CURLU_ALLOW_SPACE | CURLU_URLENCODE);
+    if (url_rc) {
         LOG_ERROR("curl_url_set for CURUPART_URL failed: %s",
-                  curl_easy_strerror(rc));
+                  curl_url_strerror(url_rc));
         goto out_easy_cleanup;
     }
     rc = curl_easy_setopt(curl, CURLOPT_CURLU, urlp);