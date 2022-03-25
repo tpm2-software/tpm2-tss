71fbe868e06ded27fa3d846a16acef4f563c9a0a
Juergen Repp <juergen.repp@sit.fraunhofer.de>
2022-03-25 07:53:49 +0100

    ci: add alpine 3.15 and ubuntu-22.04-mbedtls-3.1
    * lcov ist not available for alpine. Thus compiling with --enabable-coverage will
      only used for the action test-coverage.
    * To test version of mbedtls 3.1 thei image ubuntu-22.04-mbedtls-3.1 was added.
    
    Signed-off-by: Juergen Repp <juergen.repp@sit.fraunhofer.de>

--- .ci/docker-prelude.sh
+++ .ci/docker-prelude.sh
@@ -17,3 +17,6 @@ export LD_LIBRARY_PATH=/usr/local/lib/
 # Change to the build dir
 echo "echo changing to $DOCKER_BUILD_DIR"
 cd $DOCKER_BUILD_DIR
+
+# workaround to solve problem with unsafe directory with alpine
+git config --global --add safe.directory /workspace/tpm2-tss
