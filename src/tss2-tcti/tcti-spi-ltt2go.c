dcec28b11f0d479571a63d46d5e23cdc4ffe44e0
Paul Kissinger <paul.kissinger@letstrust.de>
2023-07-11 15:40:41 +0200

    TCTI-SPI-LTT2GO: Correct naming from ltl2go to ltt2go
    change naming in all files also filenames was corrected
    
    Signed-off-by: Paul Kissinger <paul.kissinger@letstrust.de>

--- src/tss2-tcti/tcti-spi-ltt2go.c
+++ src/tss2-tcti/tcti-spi-ltt2go.c
@@ -14,8 +14,8 @@
 #include <assert.h>
 
 #include "tss2_tcti.h"
-#include "tcti-spi-lt2go.h"
-#include "tss2_tcti_spi_lt2go.h"
+#include "tcti-spi-ltt2go.h"
+#include "tss2_tcti_spi_ltt2go.h"
 #include "tcti-spi-helper.h"
 #include "tss2_mu.h"
 #include "util/io.h"
@@ -196,7 +196,7 @@ platform_finalize(void *user_data)
 }
 
 TSS2_RC
-create_tcti_spi_lt2go_platform (TSS2_TCTI_SPI_HELPER_PLATFORM *platform)
+create_tcti_spi_ltt2go_platform (TSS2_TCTI_SPI_HELPER_PLATFORM *platform)
 {
     int ret = 0;
     int nb_ifaces = 0;
@@ -296,7 +296,7 @@ out:
 }
 
 TSS2_RC
-Tss2_Tcti_Spi_Lt2go_Init (TSS2_TCTI_CONTEXT* tcti_context, size_t* size, const char* config)
+Tss2_Tcti_Spi_Ltt2go_Init (TSS2_TCTI_CONTEXT* tcti_context, size_t* size, const char* config)
 {
     (void) config;
     TSS2_RC ret = 0;
@@ -307,7 +307,7 @@ Tss2_Tcti_Spi_Lt2go_Init (TSS2_TCTI_CONTEXT* tcti_context, size_t* size, const c
         return Tss2_Tcti_Spi_Helper_Init (NULL, size, NULL);
     }
 
-    if ((ret = create_tcti_spi_lt2go_platform (&tcti_platform))) {
+    if ((ret = create_tcti_spi_ltt2go_platform (&tcti_platform))) {
         return ret;
     }
 
@@ -317,10 +317,10 @@ Tss2_Tcti_Spi_Lt2go_Init (TSS2_TCTI_CONTEXT* tcti_context, size_t* size, const c
 
 const TSS2_TCTI_INFO tss2_tcti_info = {
     .version = TCTI_VERSION,
-    .name = "tcti-spi-lt2go",
+    .name = "tcti-spi-ltt2go",
     .description = "TCTI for communicating with LetsTrust-TPM2Go.",
     .config_help = "Takes no configuration.",
-    .init = Tss2_Tcti_Spi_Lt2go_Init
+    .init = Tss2_Tcti_Spi_Ltt2go_Init
 };
 
 const TSS2_TCTI_INFO *
