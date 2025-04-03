f31949ee30e9554892e35ab17f1c9b4abd484db6
Juergen Repp <juergen_repp@web.de>
2025-04-03 15:46:58 +0200

    TCTI: Add Tss2_Tcti_Null_Context struct
    This struct can be used for quick access without requiring to
    call Tss2_Tcti_Null_Init() first.
    Fixes: #2950
    
    Signed-off-by: Juergen Repp <juergen_repp@web.de>

--- src/tss2-tcti/tcti-null.h
+++ src/tss2-tcti/tcti-null.h
@@ -12,6 +12,11 @@
 
 #define TCTI_NULL_MAGIC 0x9af45c5d7d9d0d3fULL
 
+static const TSS2_TCTI_CONTEXT_COMMON_V2 Tss2_Tcti_Null_Context = {
+    .v1.magic = TCTI_NULL_MAGIC,
+    .v1.version = 2,
+};
+
 typedef struct {
     const char *child_tcti;
 } tcti_null_conf_t;
