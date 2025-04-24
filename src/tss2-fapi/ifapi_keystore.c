04e6e021dc871b1cf1febe99c2f9ad3679cfe305
Juergen Repp <juergen_repp@web.de>
2025-04-24 11:59:51 +0200

    FAPI: Add new profiles and initialization of the key unique field.
    Now it's possible to adjust initialization of the key unique field
    and new profiles are added.
    The unique initialization of the SRK and EK is now controlled via
    the FAPI profiles, and is enabled by new parameters in the
    key template:
    The initialization with zeros is provided e.g, by:
        unique_zero=n
    for RSA and ECC keys he unique field can b initialized with hex values:
        RSA: unique=0c0a...
        ECC: unique_x=0c0a...
             unique_y=0x0a...
    For RSA2028 and ECCP256 the following additional profiles are
    added:
    * legacy: For the FAPI legacy and systemd legacy default
      (without noda set)
    * linux: The TCG high range profile (without unique initialization
      and with noda set)
    * windows: SRK created by windows (with noda set and with
      unique initialization). Corresponds to the TCG low range profile.
    
    Signed-off-by: Juergen Repp <juergen_repp@web.de>

--- src/tss2-fapi/ifapi_keystore.c
+++ src/tss2-fapi/ifapi_keystore.c
@@ -1553,6 +1553,7 @@ ifapi_cleanup_ifapi_key(IFAPI_KEY * key) {
         SAFE_FREE(key->description);
         SAFE_FREE(key->certificate);
         SAFE_FREE(key->appData.buffer);
+        key->unique_init_set = TPM2_NO;
     }
 }
 
@@ -1668,6 +1669,9 @@ ifapi_copy_ifapi_key_object(IFAPI_OBJECT * dest, const IFAPI_OBJECT * src) {
     dest->system = src->system;
     dest->public.handle = src->public.handle;
     dest->authorization_state = src->authorization_state;
+    if (src->misc.key.unique_init_set) {
+        dest->misc.key.unique_init = src->misc.key.unique_init;
+    }
 
     return r;
 
