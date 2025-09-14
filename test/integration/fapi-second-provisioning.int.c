509bbd46675d486dca645e9f240476e4291d28c0
Juergen Repp <juergen_repp@web.de>
2025-09-14 22:07:52 +0200

    FAPI Test: Add compatibility test for old FAPI profiles.
    The test fapi-second-provisioning is extended to check whether legacy profiles
    which might still be used produce the same SRK and EK as the new profiles
    where the initialization of the unique field can be adjusted.
    
    Signed-off-by: Juergen Repp <juergen_repp@web.de>

--- test/integration/fapi-second-provisioning.int.c
+++ test/integration/fapi-second-provisioning.int.c
@@ -13,6 +13,8 @@
 
 #include "test-fapi.h"    // for init_fapi, FAPI_PROFILE, pcr_reset, EXIT_SKIP
 #include "tss2_common.h"  // for TSS2_RC, TSS2_RC_SUCCESS, TSS2_FAPI_RC_AUTH...
+#include "tss2_tpm2_types.h" // for TPM2B_PUBLIC
+#include "tss2_mu.h"      // for unmarshaling TPM2B_PUBLIC
 #include "tss2_fapi.h"    // for Fapi_Provision, Fapi_Delete, Fapi_Finalize
 
 #define LOGMODULE test
@@ -38,6 +40,62 @@ auth_callback(
     return TSS2_RC_SUCCESS;
 }
 
+static bool
+cmp_public_key(
+    TPM2B_PUBLIC *key1,
+    TPM2B_PUBLIC *key2)
+{
+    if (key1->publicArea.type != key2->publicArea.type)
+        return false;
+    switch (key1->publicArea.type) {
+    case TPM2_ALG_RSA:
+        if (key1->publicArea.unique.rsa.size != key2->publicArea.unique.rsa.size) {
+            return false;
+        }
+        LOGBLOB_TRACE(&key1->publicArea.unique.rsa.buffer[0],
+                      key1->publicArea.unique.rsa.size, "Key 1");
+        LOGBLOB_TRACE(&key2->publicArea.unique.rsa.buffer[0],
+                      key2->publicArea.unique.rsa.size, "Key 2");
+        if (memcmp(&key1->publicArea.unique.rsa.buffer[0],
+                   &key2->publicArea.unique.rsa.buffer[0],
+                   key1->publicArea.unique.rsa.size) == 0)
+            return true;
+        else
+            return false;
+        break;
+    case TPM2_ALG_ECC:
+        if (key1->publicArea.unique.ecc.x.size != key2->publicArea.unique.ecc.x.size) {
+            return false;
+        }
+        LOGBLOB_TRACE(&key1->publicArea.unique.ecc.x.buffer[0],
+                      key1->publicArea.unique.ecc.x.size, "Key 1 x");
+        LOGBLOB_TRACE(&key2->publicArea.unique.ecc.x.buffer[0],
+                      key2->publicArea.unique.ecc.x.size, "Key 2 x");
+        if (memcmp(&key1->publicArea.unique.ecc.x.buffer[0],
+                   &key2->publicArea.unique.ecc.x.buffer[0],
+                   key1->publicArea.unique.ecc.x.size) != 0)
+            return false;
+        if (key1->publicArea.unique.ecc.y.size != key2->publicArea.unique.ecc.y.size) {
+            return false;
+        }
+        LOGBLOB_TRACE(&key1->publicArea.unique.ecc.y.buffer[0],
+                      key1->publicArea.unique.ecc.y.size, "Key 1 x");
+        LOGBLOB_TRACE(&key2->publicArea.unique.ecc.y.buffer[0],
+                      key2->publicArea.unique.ecc.y.size, "Key 2 x");
+        if (memcmp(&key1->publicArea.unique.ecc.y.buffer[0],
+                   &key2->publicArea.unique.ecc.y.buffer[0],
+                   key1->publicArea.unique.ecc.y.size) != 0)
+            return false;
+        else
+            return true;
+        break;
+
+    default:
+        return false;
+    }
+}
+
+
 /** Test the FAPI provisioning with passwords already set for endorsement and
  *  owner hierarchy.
  *
@@ -55,6 +113,14 @@ int
 test_fapi_test_second_provisioning(FAPI_CONTEXT *context)
 {
     TSS2_RC r;
+
+    uint8_t *publicblob = NULL;
+    size_t  publicsize;
+    TPM2B_PUBLIC srk_public1;
+    TPM2B_PUBLIC ek_public1;
+    TPM2B_PUBLIC srk_public2;
+    TPM2B_PUBLIC ek_public2;
+    size_t offset;
 
     if (strncmp(FAPI_PROFILE, "P_RSA", 5) == 0) {
         LOG_WARNING("Default ECC profile needed for this test %s is used", FAPI_PROFILE);
@@ -147,8 +213,10 @@ test_fapi_test_second_provisioning(FAPI_CONTEXT *context)
         rc = init_fapi("P_ECC", &context);
     } else if (strcmp(FAPI_PROFILE, "P_ECC384") == 0) {
         rc = init_fapi("P_ECC384", &context);
+    } else if (strcmp(FAPI_PROFILE, "P_RSA") == 0) {
+        rc = init_fapi("P_RSA", &context);
     } else if (strcmp(FAPI_PROFILE, "P_RSA3072") == 0) {
-        rc = init_fapi("P_ECC384", &context);
+        rc = init_fapi("P_RSA3072", &context);
     } else {
         LOG_ERROR("Profile %s not supported for this test!", FAPI_PROFILE);
     }
@@ -162,12 +230,82 @@ test_fapi_test_second_provisioning(FAPI_CONTEXT *context)
 
     goto_if_error(r, "Error Fapi_Provision", error);
 
-    Fapi_Delete(context, "/");
+    r = Fapi_GetTpmBlobs(context,  "/SRK", &publicblob,
+                         &publicsize, NULL, NULL, NULL);
+    goto_if_error(r, "Error Fapi_GetTpmBlobs", error);
 
+    offset = 0;
+    r = Tss2_MU_TPM2B_PUBLIC_Unmarshal(publicblob, publicsize, &offset, &srk_public1);
+    SAFE_FREE(publicblob);
+    goto_if_error(r, "Context unmarshal", error);
+
+
+    r = Fapi_GetTpmBlobs(context,  "/EK", &publicblob,
+                         &publicsize, NULL, NULL, NULL);
+    goto_if_error(r, "Error Fapi_GetTpmBlobs", error);
+
+    offset = 0;
+    r = Tss2_MU_TPM2B_PUBLIC_Unmarshal(publicblob, publicsize, &offset, &ek_public1);
+    SAFE_FREE(publicblob);
+    goto_if_error(r, "Context unmarshal", error);
+
+    Fapi_Delete(context, "/");
+    Fapi_Finalize(&context);
+
+     /* Provisioning with legacy profiles To check whether the same SRK
+        and EK is computed.*/
+
+    if (strcmp(FAPI_PROFILE, "P_ECC") == 0) {
+        rc = init_fapi("P_ECC_no_unique_init", &context);
+    } else if (strcmp(FAPI_PROFILE, "P_ECC384") == 0) {
+        rc = init_fapi("P_ECC384_no_unique_init", &context);
+    } else if (strcmp(FAPI_PROFILE, "P_RSA") == 0) {
+        rc = init_fapi("P_RSA_no_unique_init", &context);
+    } else if (strcmp(FAPI_PROFILE, "P_RSA3072") == 0) {
+        rc = init_fapi("P_RSA3072_no_unique_init", &context);
+    } else {
+        LOG_ERROR("Profile %s not supported for this test!", FAPI_PROFILE);
+    }
+
+    if (rc)
+        goto error;
+
+    r = Fapi_Provision(context, NULL, NULL, NULL);
+    goto_if_error(r, "Error Fapi_Provision", error);
+
+    r = Fapi_GetTpmBlobs(context,  "/SRK", &publicblob,
+                         &publicsize, NULL, NULL, NULL);
+    goto_if_error(r, "Error Fapi_GetTpmBlobs", error);
+
+    offset = 0;
+    r = Tss2_MU_TPM2B_PUBLIC_Unmarshal(publicblob, publicsize, &offset, &srk_public2);
+    SAFE_FREE(publicblob);
+    goto_if_error(r, "Context unmarshal", error);
+
+    if (!cmp_public_key(&srk_public1, &srk_public2)) {
+        LOG_ERROR("Legacy SRK not equal to SRK");
+        goto error;
+    }
+
+    r = Fapi_GetTpmBlobs(context,  "/EK", &publicblob,
+                         &publicsize, NULL, NULL, NULL);
+    goto_if_error(r, "Error Fapi_GetTpmBlobs", error);
+
+    offset = 0;
+    r = Tss2_MU_TPM2B_PUBLIC_Unmarshal(publicblob, publicsize, &offset, &ek_public2);
+    SAFE_FREE(publicblob);
+    goto_if_error(r, "Context unmarshal", error);
+
+    if (!cmp_public_key(&ek_public1, &ek_public2)) {
+        LOG_ERROR("Legacy EK not equal to EK");
+        goto error;
+    }
+
+    Fapi_Delete(context, "/");
     return EXIT_SUCCESS;
 
 error:
-    Fapi_Delete(context, "/");
+    SAFE_FREE(publicblob);
     return EXIT_FAILURE;
 }
 
