f61bfe8c20c63b868cc113bc4ea698b8efe01c69
Juergen Repp <juergen_repp@web.de>
2026-08-05 19:34:52 +0200

    fapi test: fix tests failing with physical TPMs.
    The check previously assumed that the list of current FAPI paths exactly
    matched the provided checklist.
    
    However, physical TPMs may contain additional NV objects and therefore
    additional FAPI paths. These extra paths do not indicate an error.
    
    Change the comparison so that it only verifies that every entry from the
    checklist is present in the current FAPI path list. Additional paths in
    the FAPI path list are now ignored.
    
    Also one assertion is moved to ensure that a modified
    password is reset even when the test fails.
    
    Remove the endorsement certificate paths from the checklist because their
    presence and exact paths may differ when tests are run with physical TPMs.
    
    Signed-off-by: Juergen Repp <juergen_repp@web.de>

--- test/integration/fapi-quote.int.c
+++ test/integration/fapi-quote.int.c
@@ -334,11 +334,9 @@ test_fapi_quote(FAPI_CONTEXT *context) {
     ASSERT(pathlist != NULL);
     ASSERT(strlen(pathlist) > ASSERT_SIZE);
     LOG_INFO("\nPathlist: %s\n", pathlist);
-    char *check_pathlist
-        = "/" FAPI_PROFILE "/HS/SRK:/" FAPI_PROFILE "/HS:/" FAPI_PROFILE "/LOCKOUT:/" FAPI_PROFILE
-          "/HE/EK:/" FAPI_PROFILE "/HE:/" FAPI_PROFILE "/HN:/" FAPI_PROFILE
-          "/HS/SRK/mySignKey:/ext/myExtPubKey:/nv/Endorsement_Certificate/1c00002"
-          ":/nv/Endorsement_Certificate/1c0000a";
+    char *check_pathlist = "/" FAPI_PROFILE "/HS/SRK:/" FAPI_PROFILE "/HS:/" FAPI_PROFILE
+                           "/LOCKOUT:/" FAPI_PROFILE "/HE/EK:/" FAPI_PROFILE "/HE:/" FAPI_PROFILE
+                           "/HN:/" FAPI_PROFILE "/HS/SRK/mySignKey:/ext/myExtPubKey";
     ASSERT(cmp_strtokens(pathlist, check_pathlist, ":"));
     LOG_INFO("\nPathlist: %s\n", check_pathlist);
 
